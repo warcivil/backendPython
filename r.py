2a6962b418ffc4e9fa6dfb29f10b7f86d8d5ade3
warcivil <36387132+warcivil@users.noreply.github.com>
2021-06-11 21:36:41 +0500

    решение последней задачи 5 недели парсинг данных  с гитхаба

--- r.py
+++ r.py
@@ -1,14 +1,4 @@
 import requests
-from pprint import pprint
-token = "xxxxxxxxxxx"
-username = "warcivil"
-r = requests.get('https://api.github.com/user', auth=(username, token))
 
-with requests.Session() as session:
-    session.auth = (username, token)
-    response = session.get('https://api.github.com/user')
-
-#pprint(response.json())
-repos = session.get('https://api.github.com/user/repos')
-repo_list = [item for item in repos.json() if item["private"] == True]
-pprint(repo_list)
+r=requests.delete('http://localhost:8082/index', data={'a':'b'})
+print(r.text)
---
+++ settings.py
@@ -0,0 +1,2 @@
+TOKEN = "xxxxxxxxx"
+USERNAME = "warcivil"
