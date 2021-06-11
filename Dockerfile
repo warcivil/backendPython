36a7c7f2a30ec57993cc064a6a63b6fab1246da8
warcivil <36387132+warcivil@users.noreply.github.com>
2021-06-11 21:30:32 +0500

    решение последней зад

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /app
 COPY requirements.txt ./
 # --no-cache-dir
 RUN pip install  -r requirements.txt
-
+RUN pip3 install requests==2.25.1
 COPY . .
 
 # CMD [ "python", "./web_app.py" ]
--- r.py
+++ r.py
@@ -1,4 +1,14 @@
 import requests
+from pprint import pprint
+token = "xxxxxxxxxxx"
+username = "warcivil"
+r = requests.get('https://api.github.com/user', auth=(username, token))
 
-r=requests.delete('http://localhost:8082/index', data={'a':'b'})
-print(r.text)
+with requests.Session() as session:
+    session.auth = (username, token)
+    response = session.get('https://api.github.com/user')
+
+#pprint(response.json())
+repos = session.get('https://api.github.com/user/repos')
+repo_list = [item for item in repos.json() if item["private"] == True]
+pprint(repo_list)
